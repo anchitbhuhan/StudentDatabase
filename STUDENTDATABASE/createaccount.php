6ad93fe906dd4ad590cc11c8e47982a7029d4033
Akshat42 <akshatdivya03@gmail.com>
2018-10-19 01:59:50 +0530

    creatinguser

---
+++ STUDENTDATABASE/createaccount.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login|Signup</title>
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+</head>
+<body>
+	<fieldset>
+	<h1>Create Your Account</h1>
+	<p>Already have an account? <a id="sign" href="#">Sign In</a></p>	
+<form>
+	<input class="inp" type="text" name="name" placeholder="Name" autocomplete="off"><br><br>
+   <input class="inp" autocomplete="off"  type="text" name="username" placeholder="Username"><br><br>
+    <input class="inp" type="text" name="email" placeholder="E-mail"><br><br>
+    <input class="inp" type="password" name="password" placeholder="Password"><br><br>
+	 <button>Create Account</button>
+</form>
+</fieldset>
+</body>
+</html>
