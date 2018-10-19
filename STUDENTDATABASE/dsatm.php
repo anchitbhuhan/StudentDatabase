77ccb1cfa67eadba2f7c211c24651f31158f6013
Akshat42 <akshatdivya03@gmail.com>
2018-10-19 22:57:02 +0530

    new file

---
+++ STUDENTDATABASE/dsatm.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DAYANAND SAGAR ACADEMY OF TECHNOLOGY AND MANAGEMENT</title>
+<link rel="stylesheet" type="text/css" href="dashboardstyle.css">
+</head>
+<body>
+<header>
+<a id="dsatm"href=https://dsatm.edu.in/><img src="/home/akshat/dsatm-proj/dsatm_logo.png" alt="dsatm"/></a>
+<div id="etc">
+<a href="https://www.facebook.com/DSIorganization"><img class="social" src="/home/akshat/dsatm-proj/fb.png"alt="fb"/></a>
+<a href="https://twitter.com/DSIorganization"><img class="social"src="/home/akshat/dsatm-proj/twitter.png" alt=twitter/></a>
+<a href="https://plus.google.com/112592840884555805844"><img class="social" src="/home/akshat/dsatm-proj/google+.png" alt=google/><a/>
+<a href="https://www.youtube.com/user/DSIorganization"><img class="social"src="/home/akshat/dsatm-proj/youtube.png" alt=ytb/></a>
+</div>
+</header>
+
+  <ul>
+    <li id=forhover >Home</li>
+    <li id=forhover >About Us</li>
+    <li id=forhover>Approvals</li>
+    <li id=forhover>Admission</li>
+    <li id=forhover>Academics</li>
+    <li id=forhover>Research</li>
+    <li id=forhover>Campus Life</li>
+    <li id=forhover>Library</li>
+     <button type="submit" name="logout" >LOGOUT</button>
+  </ul>
+<section style="height: 500px;">
+  
+
+</section>
+
+  <footer>
+  <pre>© 2018 Dayananda Sagar Institutions . All Rights Reserved.</pre>
+  </footer>
