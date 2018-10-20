088d0acfa680e2b048d789c5e27b22f524c6f9ab
Akshat42 <akshatdivya03@gmail.com>
2018-10-20 09:52:01 +0530

    onclickaddstudent

---
+++ STUDENTDATABASE/onclickaddstudent.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DAYANAND SAGAR ACADEMY OF TECHNOLOGY AND MANAGEMENT</title>
+<link rel="stylesheet" type="text/css" href="dashboardstyle.css">
+
+</head>
+<body>
+<header>
+<a id="dsatm"href=https://dsatm.edu.in/><img src="dsatm_logo.png" alt="dsatm"/></a>
+<div id="etc">
+<a href="https://www.facebook.com/DSIorganization"><img class="social" src="fb.png"alt="fb"/></a>
+<a href="https://twitter.com/DSIorganization"><img class="social"src="twitter.png" alt=twitter/></a>
+<a href="https://plus.google.com/112592840884555805844"><img class="social" src="google+.png" alt=google/><a/>
+<a href="https://www.youtube.com/user/DSIorganization"><img class="social"src="youtube.png" alt=ytb/></a>
+</div>
+</header>
+
+  <ul>
+    <li id=forhover >Home</li>
+    <li id=forhover >About Us</li>
+    <li id=forhover>Approvals</li>
+    <li id=forhover>Admission</li>
+    <li id=forhover>Academics</li>
+    <li id=forhover>Research</li>
+    <li id=forhover>Campus Life</li>
+    <li id=forhover>Library</li>
+     <button type="submit" name="logout" >LOGOUT</button>
+  </ul>
+<section style="height: 500px;">
+   <fieldset>
+    <form class="" action="includes/pdsubmit.php" method="post"  enctype="multipart/form-data">
+      <input type="text" name="usn" placeholder="USN"><br><br>
+
+      <input type="text" name="name" placeholder="Name"><br><br>
+
+      <input type="text" name="mobile" placeholder="Mobile"><br><br>
+
+      DOB: <input type="date" name="dob" placeholder="DOB"><br><br>
+
+      HOSTELITE:YES<input type="radio" name="hostelite" value="yes" ><br><br>
+                    NO:<input type="radio" name="hostelite" value="no"><br><br>
+      GENDER:Male<input type="radio" name="gender" value="male"><br><br>
+      Female<input type="radio" name="gender" value="female"><br><br>
+
+      <input type="text" name="address" placeholder="address"><br><br>
+      Upload Image: <input type="file" name="image"><br><br>
+      <input type="submit" name="submit"><br>
+    </form>
+</fieldset>
+</section>
+
+  <footer>
+  <pre>© 2018 Dayananda Sagar Institutions . All Rights Reserved.</pre>
+  </footer>
