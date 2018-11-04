c01447565c63173534fa7eae0d66bf2aa2bc5c2f
Akshat42 <akshatdivya03@gmail.com>
2018-11-04 16:02:40 +0530

    dekh ke bata

---
+++ fieldsetPD.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		fieldset{
+			width: 50%;
+			margin: 0;
+			height: calc(100vh - 80px);
+			background-color:#a7c4e8;
+			border: none;
+			padding: 0;
+		}
+		h1{
+			margin:0;
+			text-align: center;
+			font-family: sans-serif;
+			border-bottom: 3px solid black;
+			background-color: #003366;
+			color: white;
+			padding-top: 2px;
+
+
+		}
+		img{
+			float: right;
+			padding: 4px;
+			padding-right: 9em; 
+			padding-top: 2em;
+		}
+		.personal{
+			margin-left: 40px; 
+			font-size: 17px;
+			margin-top: 6px;
+			font-family: sans-serif;
+			
+		}
+	</style>
+</head>
+<body>
+<fieldset>
+	<h1>PERSONAL DETAILS</h1><br>
+	<img src="favicon.ico" alt="st_img">
+	<p class="personal"><b>USN:</b><?php echo " 1DT16IS019" ?></p><br>
+	<p class="personal"><b>Name:</b><?php echo " Anchit Bhushan" ?></p><br>
+	<p class="personal"><b>Mobile:</b><?php echo " 9431118976" ?></p><br>
+	<p class="personal"><b>DoB:</b><?php echo " 19/07/1998" ?></p><br>
+	<p class="personal"><b>Hostelite:</b><?php echo " No" ?></p><br>
+	<p class="personal"><b>Gender:</b><?php echo " Chakka" ?></p><br>
+	<p class="personal"><b>Address:</b><?php echo " Hazaribagh,Jharkhand" ?></p><br>
+</fieldset>
+</body>
+</html>
