ccbb9b94b1e77f47ae8362bf602e6a9a34387cb4
Akshat42 <akshatdivya03@gmail.com>
2018-11-04 19:14:15 +0530

    dekh ke bata

---
+++ fieldsetAch.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		fieldset{
+			width: 100%%;
+			margin: 0;
+			height: calc(100vh - 80px);
+			background-color:#a7c4e8;
+			border: none;
+			padding: 0;
+		}
+		h1{
+			margin:0;
+			text-align: center;
+			font-family: sans-serif;
+			border-bottom: 3px solid black;
+			background-color: #003366;
+			color: white;
+			padding-top: 2px;
+	}
+	
+		.personal{
+			margin-left: 40px; 
+			font-size: 22px;
+			margin-top: 8px;
+			font-family: sans-serif;
+		}
+		table,th,tr,td{
+			border: 1px solid black;
+			border-collapse: collapse;
+			padding: 8px;
+			text-align: center;
+			font-family:sans-serif;
+			margin-left: 5em;
+			margin-top: 3em;
+		}
+		th{
+			background-color: #003366;
+			color: white;
+		}
+	</style>
+</head>
+<body>
+<fieldset>
+	<h1>COURSE DETAILS</h1><br>
+	
+<p class="personal"><b>USN:</b><?php echo " 1DT16IS019" ?></p>
+<p class="personal"><b>NAME:</b><?php echo " Anchit Bhushan" ?></p>
+<table>
+	<tr>
+		<th>PROJECTS</th>
+		<th>INTERNSHIPS	</th>
+		<th>CERTIFICATIONS</th>
+		<th>PAPER PRESENTATION</th>
+	</tr>
+	<tr>
+		<td>Student Database</td>
+		<td>Amazon</td>
+		<td>Sun Certification,Python Developer(Sab kar liya)</td>
+		<td>None</td>
+		
+	</tr>
+</table>
+
+</fieldset>
+</body>
+</html>
